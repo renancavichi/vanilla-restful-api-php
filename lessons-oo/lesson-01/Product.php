fbe59cda4e777486606b6f964a64c3bcd42732e9
Renan Cavichi <renancavichi@gmail.com>
2022-08-09 17:58:05 -0300

    add lesson 1 - class, methods, constructor, properties and objects

---
+++ lessons-oo/lesson-01/Product.php
@@ -0,0 +1,35 @@
+<?php
+class Product{
+    // Properties
+    public $id;
+    public $image;
+    public $name; 
+    public $price; 
+
+    // Construct Method 
+    function __construct($id, $image, $nome, $price) {
+        $this->id = $id;
+        $this->image = $image;
+        $this->nome = $name;
+        //$this->setName($name);
+        $this->price = $price;
+    }
+
+    // Methods (Functions)
+    function setPrice($price){
+        $this->price = $price;
+    }
+
+    function getPrice(){
+        echo $this->price;
+    }
+
+    function setName($name){
+        $this->name = str_replace("'", "°", $name);
+    }
+
+    function getName(){
+        echo $this->name;
+    }  
+}
+?>
---
+++ lessons-oo/lesson-01/oo-lesson1.php
@@ -0,0 +1,26 @@
+<?php
+// Lesson 1: Class, Objects, Constructor, Properties and Methods
+
+require('Product.php');
+
+$prod = new Product(null, null, null, null);
+
+$prod->setPrice(10);
+$prod->setName("TV 40' Polegadas");
+
+echo "Valor = ";
+$prod->getPrice();
+
+echo "<br>Nome = ";
+$prod->getName();
+
+echo "<br>----------------<br>";
+
+$prod2 = new Product(null, null, "TV 50'", 50);
+
+echo "Valor = ";
+$prod2->getPrice();
+
+echo "<br>Nome = ";
+$prod2->getName();
+?>
