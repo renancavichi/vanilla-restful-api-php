f61ae27737fae721c764fd2b2f76ae3c25a24fc3
Renan Cavichi <renancavichi@gmail.com>
2022-08-30 17:39:13 -0300

    refactoring and add output json

--- router.php
+++ router.php
@@ -13,19 +13,16 @@ if(isset($urlArray[0]) && $urlArray[0] != '' &&
    isset($urlArray[1]) && $urlArray[1] != ''
 ){
     $controller = $urlArray[0].'Controller';
-    $action = $urlArray[1];
-} else{
-    echo 'Endereço da API inválido!';
-    die;
-}
-
-if(file_exists(CONTROLLERS_FOLDER.$controller.'.php')){
-    $obj = new $controller();
-    if(method_exists($obj, $action)){
-        $obj->$action();
-        die;
+    $action = str_replace('-', '', $urlArray[1]);
+    if(file_exists(CONTROLLERS_FOLDER.$controller.'.php')){
+        $obj = new $controller();
+        if(method_exists($obj, $action)){
+            $obj->$action();
+            die;
+        }
     }
-}
+} 
 
-echo 'Endereço da API inválido!';
+$output = new Output();
+$output->notFound();
 ?>
