ec2da51504e824f7c622f4baa7622da5e7ac8a38
Renan Cavichi <renancavichi@gmail.com>
2022-08-30 17:39:29 -0300

    add sign up action

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,10 +1,29 @@
 <?php
 class UserController{
-    function add(){
-        echo 'Add user';
-    }
-    function delete(){
-        echo 'Delete user';
+    function signup(){
+        //pegar as entradas
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $pass = sha1($_POST['pass']);
+        $avatar = $_POST['avatar'];
+        //validar os campos
+
+        //executar a operacao de banco
+        $user = new User(null, $name, $email, $pass, $avatar);
+        $id = $user->create();
+
+        //dar a saida
+        $result["success"]["message"] = "User created successfully!";
+
+        $result["user"]["id"] = $id;
+        $result["user"]["name"] = $name;
+        $result["user"]["email"] = $email;
+        $result["user"]["pass"] = $pass;
+        $result["user"]["avatar"] = $avatar;
+
+
+        $output = new Output();
+        $output->response($result);
     }
 }
 ?> 
