d03cb201e44312e5ad83243c253d92627c7219d4
Renan Cavichi <renancavichi@gmail.com>
2022-08-30 17:39:58 -0300

    add helper and model autoloader

--- helpers/autoloaders.php
+++ helpers/autoloaders.php
@@ -3,6 +3,12 @@ function myLoaders($class_name){
     if(file_exists(CONTROLLERS_FOLDER . $class_name . '.php')){
         require CONTROLLERS_FOLDER . $class_name . '.php';
     }
+    if(file_exists(HELPERS_FOLDER . $class_name . '.php')){
+        require HELPERS_FOLDER . $class_name . '.php';
+    }
+    if(file_exists(MODELS_FOLDER . $class_name . '.php')){
+        require MODELS_FOLDER . $class_name . '.php';
+    }
 }
 //automatiza o carregamento das classes
 spl_autoload_register('myLoaders');
