ccf6f164c7ad0184fd2736ce29e52be341959f94
Renan Cavichi <renancavichi@gmail.com>
2022-08-30 17:40:56 -0300

    create user model

---
+++ models/User.php
@@ -0,0 +1,40 @@
+<?php
+class User {
+    
+    private $id;
+    private $name;
+    private $email;
+    private $pass;
+    private $avatar;
+
+    function __construct($id, $name, $email, $pass, $avatar) {
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->pass = $pass;
+        $this->avatar = $avatar;
+    }
+
+    function create(){
+        $db = new Database();
+        $conn = $db->connect();
+        
+        try{
+            $stmt = $conn->prepare("INSERT INTO users (name, email, pass, avatar)
+            VALUES (:name, :email, :pass, :avatar)");
+            $stmt->bindParam(':name', $this->name);
+            $stmt->bindParam(':email', $this->email);
+            $stmt->bindParam(':pass', $this->pass);
+            $stmt->bindParam(':avatar', $this->avatar);
+            $stmt->execute();
+            $id = $conn->lastInsertId();
+            $conn = null;
+            return $id;
+        }catch(PDOException $e) {
+            $db->dbError($e);
+        }
+    }
+
+}
+
+?>
