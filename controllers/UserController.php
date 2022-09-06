a5ed2456919b88308a13059462bc8a5413a0d8ab
Renan Cavichi <renancavichi@gmail.com>
2022-09-06 17:14:20 -0300

    add allowed method

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -1,18 +1,22 @@
 <?php
 class UserController{
     function signup(){
+        $route = new Router();
+        $route->allowedMethod('POST');
+
         //pegar as entradas
         $name = $_POST['name'];
         $email = $_POST['email'];
         $pass = sha1($_POST['pass']);
         $avatar = $_POST['avatar'];
         //validar os campos
+        //TODO
 
         //executar a operacao de banco
         $user = new User(null, $name, $email, $pass, $avatar);
         $id = $user->create();
 
-        //dar a saida
+        //dar a saída
         $result["success"]["message"] = "User created successfully!";
 
         $result["user"]["id"] = $id;
@@ -21,6 +25,19 @@ class UserController{
         $result["user"]["pass"] = $pass;
         $result["user"]["avatar"] = $avatar;
 
+        $output = new Output();
+        $output->response($result);
+    }
+
+    function list(){
+        $route = new Router();
+        $route->allowedMethod('GET');
+
+        $user = new User(null, null, null, null, null);
+        $listUsers = $user->list();
+
+        $result["success"]["message"] = "User list has been successfully listed!";
+        $result["data"] = $listUsers;
 
         $output = new Output();
         $output->response($result);
