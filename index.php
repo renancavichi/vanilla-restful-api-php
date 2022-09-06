3fe32891a744c9e1f33b60acd73b24f373302459
Renan Cavichi <renancavichi@gmail.com>
2022-09-06 17:13:27 -0300

    refactoring route to use index.php

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php 
-    require('router.php');
+<?php
+//importa o arquivo de configuracao
+require 'config.php';
+require HELPERS_FOLDER.'autoloaders.php';
+
+$route = new Router();
+$route->gateKeeper();
+
+$output = new Output();
+$output->notFound();
 ?>
