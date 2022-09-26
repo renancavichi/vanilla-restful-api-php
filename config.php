dc626e21cf2d3c5895e6cce0f2bcee1ef35b280d
Renan Cavichi <renancavichi@gmail.com>
2022-09-26 17:11:04 -0300

    add database constants to config

--- config.php
+++ config.php
@@ -1,8 +1,15 @@
 <?php
+// PATHS and FOLDERS
 define('BASE_PATH','/pj3/api-php-ifsp-2022-2/');
 define('CONTROLLERS_FOLDER','controllers/');
 define('HELPERS_FOLDER','helpers/');
 define('MODELS_FOLDER','models/');
+
+//DATABASE
+define('DB_NAME','renan2022');
+define('DB_USER','root');
+define('DB_HOST','localhost');
+define('DB_PASS','');
 
 // CORS
 define('ALLOWED_HOSTS', 'http://localhost:3000');
--- helpers/Database.php
+++ helpers/Database.php
@@ -1,13 +1,8 @@
 <?php 
 class Database{
     static function connect(){
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "renan2022";
-        
         try {
-          $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+          $conn = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME, DB_USER, DB_PASS);
           // set the PDO error mode to exception
           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           return $conn;
